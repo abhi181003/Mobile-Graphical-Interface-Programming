8727a7d4cab08ad0d95750f116c41cf9da217a08
abhi181003 <148674924+abhi181003@users.noreply.github.com>
2024-01-29 17:41:12 -0500

    Added compound buttons and ImageView along with toasts

--- app/src/main/java/algonquin/cst2335/agga0042/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/agga0042/MainActivity.java
@@ -22,9 +22,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(variableBinding.getRoot());
 
         Button btn = variableBinding.mybutton;
+        CheckBox checkBox = variableBinding.checkBox;
+        RadioButton radioButton = variableBinding.radioButton;
+        Switch Switch = variableBinding.Switch;
         ImageView imageView = variableBinding.Image;
 
+        btn.setOnClickListener(click ->
+        {
+            model.editString.postValue(variableBinding.myedittext.getText().toString());
 
+        });
+
+        model.getEditBoolean().observe(this, editBoolean-> {
+            checkBox.setChecked(editBoolean);
+            radioButton.setChecked(editBoolean);
+            Switch.setChecked(editBoolean);
+        });
+
+        variableBinding.checkBox.setOnCheckedChangeListener((compoundButton, isChecked) ->
+                model.setEditBoolean(isChecked));
+        variableBinding.radioButton.setOnCheckedChangeListener((compoundButton, isChecked) ->
+                model.setEditBoolean(isChecked));
+        variableBinding.Switch.setOnCheckedChangeListener((compoundButton, isChecked) ->
+                model.setEditBoolean(isChecked));
+
+        model.editString.observe(this, isChecked -> {
+            String text = "The value is now: " + isChecked;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast.makeText(this, text, duration).show();
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int width = imageView.getWidth();
+                int height = imageView.getHeight();
+                String msg = "The width = "+width+" and height = "+height;
+                int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(MainActivity.this, msg, duration).show();
+            }
+        });
     }
 
 }
