c5329405f2763dd9c300cf8585ba4c121f900658
abhi181003 <148674924+abhi181003@users.noreply.github.com>
2024-02-03 11:38:58 -0500

    week 2 last commit

--- app/src/main/java/algonquin/cst2335/agga0042/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/agga0042/ui/MainActivity.java
@@ -1,13 +1,11 @@
-package algonquin.cst2335.agga0042;
+package algonquin.cst2335.agga0042.ui;
 
-import androidx.lifecycle.MutableLiveData;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.*;
-import algonquin.cst2335.agga0042.R;
+
 import algonquin.cst2335.agga0042.databinding.ActivityMainBinding;
 import algonquin.cst2335.agga0042.data.MainViewModel;
 
@@ -29,11 +27,16 @@ public class MainActivity extends AppCompatActivity {
 
         btn.setOnClickListener(click ->
         {
-
-            model.editString.observe(this,s -> {
-                variableBinding.textview.setText("Your edit text has: " + s);
-            });
             model.editString.postValue(variableBinding.myedittext.getText().toString());
+
+            String text = "The value is now: " + variableBinding.myedittext.getText().toString();
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(this, text, duration).show();
+
+        });
+
+        model.editString.observe(this,s -> {
+            variableBinding.textview.setText("Your edit text has: " + s);
         });
 
         model.getEditBoolean().observe(this, editBoolean-> {
@@ -56,14 +59,6 @@ public class MainActivity extends AppCompatActivity {
         };
 
         variableBinding.checkBox.setOnCheckedChangeListener(compoundButtonListener);
-
-
-        model.editString.observe(this, isChecked -> {
-            String text = "The value is now: " + isChecked;
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast.makeText(this, text, duration).show();
-        });
 
         imagebutton.setOnClickListener(new View.OnClickListener() {
             @Override
