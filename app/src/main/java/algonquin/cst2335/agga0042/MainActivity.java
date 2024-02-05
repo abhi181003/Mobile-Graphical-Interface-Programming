648442ffb0c2923b302840eaa0b40059d4889843
abhi181003 <148674924+abhi181003@users.noreply.github.com>
2024-02-05 17:53:23 -0500

    Week 4 first commit saving progress

--- app/src/main/java/algonquin/cst2335/agga0042/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/agga0042/MainActivity.java
@@ -3,12 +3,47 @@ package algonquin.cst2335.agga0042;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.w( TAG, "In onCreate() - Loading Widgets" );
+        Log.w( TAG, "onCreate() is the first function that gets created when an application is launched." );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( TAG, "onStart() - The application is now visible on screen." );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( TAG, "onResume() - The application is now responding to user input" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( TAG, "onPause()- The application no longer responds to user input" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( TAG, "onStop() - The application is no longer visible." );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( TAG, "onDestroy() - Any memory used by the application is freed" );
     }
 }
